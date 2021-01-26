8372fcaa2e8c04f845aad6bc8ffec573a3a2b2f6
Leandro Parrado <lparrado321@gmail.com>
2021-01-26 15:42:40 -0500

    Exported module

--- route/htmlRoutes.js
+++ route/htmlRoutes.js
@@ -6,9 +6,11 @@ router.get('/notes', function (req, res) {
     res.sendFile(path.join(__dirname, "../public/assets/notes.html"))
   });
 
-//Returns the notes.html file using GET
+//Returns the index.html file using GET
 router.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, "../public/assets/index.html"))
   });
+
+  module.export = router;
 
 
