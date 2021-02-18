ac3263d6f332581ff35d53d8e440ed4d12605cde
Leandro Parrado <lparrado321@gmail.com>
2021-02-17 19:55:49 -0500

    Finaly got the html routes to work! Woohoo!

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,13 +4,15 @@ const path = require('path');
 
 //Returns the notes.html file using GET
 router.get('/notes', function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/assets/notes.html"))
+    res.sendFile(path.join(__dirname, "../public/notes.html"))
 });
 
 //Returns the index.html file using GET
-router.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/assets/index.html"))
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"))
 });
+
+// app.use(require("../public/notes.html"))
 
 
 module.exports = router;
