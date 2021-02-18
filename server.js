30196aea4daab2c3c6dda3b567514601cd534c9e
Leandro Parrado <lparrado321@gmail.com>
2021-02-17 19:26:50 -0500

    Required routes in server.js

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
+//Dependencies
 const express = require("express");
 
+//Creating an instance of express
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+//Require routes and use.
+app.use(require("./routes/htmlRoutes"));
+app.use(require("./routes/apiRoutes"))
