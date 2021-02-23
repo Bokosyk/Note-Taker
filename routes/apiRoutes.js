48e6d7ccf1500ba2518bda8955260bd9105dd9e9
Leandro Parrado <lparrado321@gmail.com>
2021-02-22 23:12:06 -0500

    Changed into a function

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,15 +2,25 @@ const router = require('express').Router();
 const notes = require("../db/db.json")
 const fs = require("fs");
 
-// let notes = [];
+module.exports = function(router){
+
+function writeToDB(data){
+    // Converts new JSON Array back to string
+    data = JSON.stringify(data);
+    // Writes String back to db.json
+    fs.writeFileSync("./db/db.json", data, function(err){
+        if (err) {
+            return console.log(err);
+        }
+    });
+}
+
+
 
 //Should read the db.json file and return all saved notes as JSON
 router.get("/api/notes", (req, res) => {
     res.json(notes);
-    // //Reads json file
-    // notes = fs.readFileSync("../db/db.json", "utf8");
-    // notes = JSON.parse(notes);
-    // // res.json(db.json)
+
 });
 
 //SHould recieve a new note to save on the request body, add to db.json file, then return new note to the client.
@@ -21,6 +31,11 @@ router.post("/api/notes", (req, res) => {
     } else {
         req.body.id = JSON.stringify(JSON.parse(notes[notes.length - 1].id) + 1);
     }
+
+
+    notes.push(req.body);
+    writeToDB(notes);
+    res.json(req.body);
 })
 
 //Should receive query parameter containing id of note to delete (Means you'll have to give each note a unique id). In order to delete notes, you'll need to read all notes from db.json file, remove note with given id property, then rewrite notes to the db.json file
@@ -34,4 +49,5 @@ router.delete("/api/notes?:id", (req, res) => {
         })
 })
 
-module.exports = router;
+writeToDB(notes);
+} //End of Line
